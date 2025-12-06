19e28474f45b18401475130272d5f7437d2343ef
Karolwyszczelski <pewnietomarka@gmail.com>
2025-12-06 01:59:04 +0100

    LEGGAL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,8 +9,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "",
     "/#menu",
     "/#kontakt",
-    "/legal/regulamin",
-    "/legal/polityka-prywatnosci",
+    "[city]/regulamin",
+    "[city]/polityka-cookies",
+    "[city]/polityka-prywatnosci",
   ];
 
   return paths.map((p) => ({
